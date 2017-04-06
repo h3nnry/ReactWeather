ce5df160767b63eeb0a4f4a4ad9ad84fe8fe1dc9
Lungu Andrei <h3nnry84@gmail.com>
2017-04-06 23:36:29 +0300

    Added error model, updated axios, catchin erro in axios promiss

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,20 +1,22 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?&units=metric&appid=c5fa7c30e0ae939b222bb92874b97e79';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
 
 module.exports = {
     getTemp: function (location) {
         var encodedLocation = encodeURIComponent(location);
         var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
-        
+
         return axios.get(requestUrl).then(function (res) {
-            if(res.data.cod && res.data.message) {
+            if (res.data.cod && res.data.message) {
                 throw new Error(res.data.message);
             } else {
                 return res.data.main.temp;
             }
         }, function (res) {
             throw new Error(res.data.message);
+        }).catch(function (error) {
+            throw new Error(error);
         });
     }
 }
